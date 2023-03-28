5b56b194310c806245422f72652507cd5f03b9d3
yunhyun <skingsol@naver.com>
2023-03-28 18:20:05 +0900

    myshop연동

--- javadb.sql
+++ javadb.sql
@@ -1,1 +1,65 @@
 -- javadb
+
+-- userTBL 테이블 생성
+-- NO(번호 - 숫자(4)), username(이름-한글4자리), birthYear(년도- 숫자4자리), 
+-- addr(주소-문자(한글,숫자), mobile(010-1234-1234)
+-- no pk 제약조건 지정(제약조건명 pk_userTBL)
+CREATE TABLE userTBL(
+        no NUMBER(4)CONSTRAINT pk_userTBL PRIMARY KEY,
+        username NVARCHAR2(10) NOT NULL,
+        birthYear NUMBER(4) NOT NULL,
+        addr NVARCHAR2(50) NOT NULL,
+        mobile VARCHAR2(13)
+);
+        
+        
+
+-- 시퀀스 생성
+-- user_seq 생성(기본)
+CREATE SEQUENCE user_seq;
+
+-- INSERT
+INSERT INTO userTBL(no, username, birthYear, addr, mobile)
+VALUES ('0047', '히트맨', '1995', '서울 종로구 종로12길 15 코아빌딩9층 901호', '010-1010-1010');
+
+COMMIT;
+
+-- paytype : no(숫자-1), info(문자-영어4)
+-- paytype_seq 생성
+CREATE TABLE paytype (
+        pay_no NUMBER(1) PRIMARY KEY,
+        info varchar2(10) NOT NULL
+);
+
+CREATE SEQUENCE paytype_seq;
+
+INSERT INTO paytype VALUES(paytype_seq.NEXTVAL, 'card');
+INSERT INTO paytype VALUES(paytype_seq.NEXTVAL, 'cash');
+
+SELECT * FROM paytype; --1: card, --2: cash
+
+-- suser : user_id(숫자-4), name(문자-한글4), pay_no(숫자-1 : paytype 테이블에 있는 pay_no 참조해서 사용)
+CREATE TABLE suser (
+        user_id NUMBER(4) PRIMARY KEY,
+        name NVARCHAR2(20) NOT NULL,
+        pay_no NUMBER(1) NOT NULL REFERENCES paytype(pay_no)
+);
+
+-- product
+-- product_id(숫자-8), pname(상품네임 문자-20), price(숫자-10), content(문자-50)
+CREATE TABLE product (
+        product_id NUMBER(8) PRIMARY KEY,
+        pname VARCHAR2(30) NOT NULL,
+        price NUMBER(10) NOT NULL,
+        content VARCHAR2(50) NOT NULL
+);
+CREATE SEQUENCE product_seq;
+
+-- sorder
+-- order_id(숫자-8), user_id(user 테이블의 user_id 참조), product_id(product 테이블에 있는 product_id 참조해서 사용)
+CREATE TABLE sorder (
+        order_id NUMBER(8) PRIMARY KEY,
+        user_id NUMBER(8) NOT NULL REFERENCES suser(user_id),
+        product_id NUMBER(8) NOT NULL REFERENCES product(product_id)
+);
+CREATE SEQUENCE order_seq;
--- system.sql
+++ system.sql
@@ -3,15 +3,27 @@
 --
 -- 오라클 버전이 변경되면서 사용자 생성 시 C## 구별 문자를 넣어서 만들도록 변경 됨
 -- JAVADB => C## JAVADB 이런 방식을 C##을 사용하지 않겠음
+
+-- 사용자 : scott, hr => 데이터베이스 접속하여 데이터를 관리하는 계정
+--         테이블, 인덱스, 뷰 등 여러 객체가 사용자별로 생성됨
+--         업무에 따라서 사용자들을 나눠서 관리
+
+-- 스키마 : 데이터간 관계, 데이터 구조, 제약조건 등 데이터를 관리 저장하기 위해 정의한 데이터베이스 구조의 범위
+-- 스키마 = 사용자 (오라클 데이터베이스의 경우에서는)
+
+-- 사용자 생성 : 사용자 생성 권한이 필요함 ==> sys, system
+
+-- 오라클 버전이 올라가면서 사용자 생성을 위해 요구하는 사항이 많아짐
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE; 
 
 -- user 생성은 sys, system 만 가능
 -- user 생성 구문 ( 사용한 공간도 할당)
 -- CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
+-- 비밀번호는 대소문자 구분함
 CREATE USER JAVADB IDENTIFIED BY 12345
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP;
-
+ALTER USER JAVADB QUOTA 2M ON USERS;
 -- GRANT : 권한 부여 (CONNECT와 RESOURCE 권한을 주는 구문) : 사용자 생성만으로는 할 수 있는게 없음
 GRANT CONNECT, RESOURCE TO JAVADB;
 
@@ -39,3 +51,57 @@ GRANT UNLIMITED TABLESPACE TO SCOTT;
 GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
 ALTER USER SCOTT DEFAULT TABLESPACE USERS;
 ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
+
+-- test 사용자 생성/ 비번12345
+CREATE USER test IDENTIFIED BY 12345;
+
+-- 접속권한 부여하지 않으면 안됨
+
+-- 권한 권리
+-- 1) 시스템 권한
+--      사용자 생성(CREATE USER) / 수정(ALTER USER) / 삭제(DROP USER)
+--      데이터베이스 접근 (CREATE SESSION) / 수정(ALTER SESSION)
+--      여러 객체 생성(view, synonym) 및 관리 권한
+
+-- 2) 객체 권한
+--      테이블 수정, 삭제, 인덱스 생성, 삽입, 참조, 조회, 수정
+--      뷰 삭제, 삽입, 생성, 조회, 수정
+--      시퀀스 수정, 조회
+--      프로지서, 함수, 패키지 권한
+
+-- 권한 부여
+
+-- CREATE SESSION 을 test 에게 부여
+GRANT CREATE SESSION TO test;
+
+GRANT RESOURCE, CREATE TABLE TO test;
+
+-- 테이블 스페이스 USERS에 권한이 없습니다.
+ALTER USER test DEFAULT TABLESPACE USERS;
+ALTER USER test TEMPORARY TABLESPACE TEMP;
+ALTER USER test QUOTA 2M ON USERS; -- USERS 에 공간 설정
+
+-- scott 에게 test가 생성한 member 테이블 조회 권한 부여
+--GRANT SELECT ON number TO scott;
+
+-- 권한 취소
+-- REVOKE SELECT, INSERT ON member FROM scott;
+
+-- 롤 : 여러 권한들의 모임
+-- CONNECT 롤 : CREATE SESSION
+-- RESOURCE 롤 : CREATE TRIGGER, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TABLE, ...
+
+
+
+
+-- 사용자 삭제
+DROP USER test;
+DROP USER test CASCADE; --test와 관련된 모든 객체 같이 삭제
+
+-- 사용자 생성 + 테이블 스페이스 권한 부여
+CREATE USER test2 IDENTIFIED BY 12345
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
+
+-- 권한 부여
+GRANT CONNECT, RESOURCE TO test2;
